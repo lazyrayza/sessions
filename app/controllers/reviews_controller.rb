e55a27cfa6e14bfec8d0a944f1d7feef267e13f3
Luis <sdaewa22@gmail.com>
2019-03-05 00:38:37 +0000

    reviews basics

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,27 @@ class ReviewsController < ApplicationController
   def index
   end
 
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+
+    if @review.save
+      redirect_to @review
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+
+  def review_params
+    params.require(:review).permit(:title, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
 
   resources :users do
     resources :bookings, only: [:new, :create]
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: [:new, :create, :show]
   end
-
+  resources :reviews, only: [:show]
   resources :request
   resources :chat_room
   resources :chatroom_participants
