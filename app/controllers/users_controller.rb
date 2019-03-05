7935f03c9b4f0a6c221b0ec32664210411940741
Ricardo Vieira <ricmvieira@gmail.com>
2019-03-05 16:25:47 +0000

    search ajax implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,12 @@ class UsersController < ApplicationController
 
   def index
     if params[:query].present?
-      @therapists = User.search_by_expertise_and_full_name(params[:query]).where.not(latitude: nil, longitude: nil).where(therapist: true)
+      @therapists = User.search_by_expertise_and_full_name(params[:query])
+      respond_to do |format|
+        format.html { render users_path }
+        format.js
+      end
+      # @therapists = User.search_by_expertise_and_full_name(params[:query]).where.not(latitude: nil, longitude: nil).where(therapist: true)
     else
       @therapists = User.where(therapist: true).where.not(latitude: nil, longitude: nil)
     end
