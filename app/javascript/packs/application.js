24893b0916cf0401f6b6847c6ad6c26f4be26a6b
MohamedRaza Pirbhai <razaP_13@live.com>
2019-03-05 12:42:34 +0000

    geocoding done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,9 @@
 import "bootstrap";
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
+
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+// import { loadDynamicBannerText } from '../components/banner';
+// loadDynamicBannerText();
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   has_many :therapist_bookings, class_name: :Booking, foreign_key: "therapist_id", dependent: :destroy
   has_many :client_bookings, class_name: :Booking, foreign_key: "client_id", dependent: :destroy
   has_many :reviews, class_name: :Review, foreign_key: "therapist_id", through: :bookings
