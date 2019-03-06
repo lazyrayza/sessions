9a9eb52951059d2719e358f366499d378a7441e5
Phuong To <tophu95@gmail.com>
2019-03-06 12:52:19 +0000

    started new scss for user login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Rails.application.routes.draw do
- devise_for :users
- root to: 'pages#home'
+devise_for :users
+root to: 'pages#home'
 
-  resources :users, only: [:show, :index] do
-    resources :bookings, only: [:new, :create]
-    resources :reviews, except: [:destroy, :edit, :update]
-  end
-
-   resources :reviews, only: [:show]
-   resources :request
-   resources :chatroom_participants
-   resources :chat_rooms, only: [ :show, :destroy ]  do
-    resources :messages, only: [ :create ]
-    resources :bookings, except: [:new, :create]
+ resources :users, only: [:show, :index] do
+   resources :reviews, except: [:destroy, :edit, :update]
+   resources :bookings, only: [:new, :create]
+ end
+ resources :reviews, only: [:show]
+ resources :request
+ resources :chatroom_participants
+ resources :chat_rooms, only: [ :show, :destroy ]  do
+   resources :messages, only: [ :create ]
+ end
+ resources :bookings, except: [:new, :create] do
+     resources :chat_rooms, only: [:new, :create]
    end
- get "/profile", to: "pages#profile"
-end
 
+ get "/profile", to: "pages#profile"
+
+
+end
