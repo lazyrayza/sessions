ea22268e2c8f0c9f29a843280ef9e0d21d05ea7a
MohamedRaza Pirbhai <razaP_13@live.com>
2019-03-06 17:23:07 +0000

    fixed chat

--- app/channels/video_session_channel.rb
+++ app/channels/video_session_channel.rb
@@ -4,9 +4,4 @@ class VideoSessionChannel < ApplicationCable::Channel
     stream_from "video_session_channel"
     # stream_from "chat_room_#{params[:chat_room_id]}"
   end
-
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
 end
