bfc81a7f1e8a8a406294869cd82b401ed5aa0d46
MohamedRaza Pirbhai <razaP_13@live.com>
2019-03-06 17:10:27 +0000

    fixed ONE issue for chat

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,6 +1,7 @@
 class ChatRoomsController < ApplicationController
   def show
     @random_number = rand(0...10_000)
+
     @chatrooms = ChatRoom.includes(messages: :user).find(params[:id])
     unless current_user == @chatrooms.booking.therapist || current_user == @chatrooms.booking.client
       redirect_to root_path
