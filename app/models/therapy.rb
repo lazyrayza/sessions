31b4b15c9c928485d56722dd1cd64418a0866a68
Luis <sdaewa22@gmail.com>
2019-03-07 15:16:51 +0000

    payment from beginning

--- app/models/therapy.rb
+++
@@ -1,2 +0,0 @@
-class Therapy < ApplicationRecord
-end
--- db/migrate/20190306214738_create_therapies.rb
+++
@@ -1,9 +0,0 @@
-class CreateTherapies < ActiveRecord::Migration[5.2]
-  def change
-    create_table :therapies do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
