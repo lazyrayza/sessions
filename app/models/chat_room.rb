886f2d2b9e5265b7c92a6281bc153b21d99ba895
MohamedRaza Pirbhai <razaP_13@live.com>
2019-03-08 15:30:15 +0000

    still fixing bugs

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -1,7 +1,5 @@
 class ChatRoom < ApplicationRecord
-  has_many :chatroom_participants
   has_many :users, through: :chatroom_participants
-  has_many :requests
   has_many :messages, dependent: :destroy
   belongs_to :booking, optional: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_08_111836) do
+ActiveRecord::Schema.define(version: 2019_03_08_131620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,12 +93,15 @@ ActiveRecord::Schema.define(version: 2019_03_08_111836) do
     t.string "phone_number"
     t.string "languages"
     t.string "expertise"
-    t.text "bookings"
+    t.bigint "booking_id"
     t.string "photo"
     t.boolean "therapist"
     t.float "latitude"
     t.float "longitude"
     t.string "price", default: "£0"
+    t.string "gender"
+    t.text "bio"
+    t.index ["booking_id"], name: "index_users_on_booking_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
