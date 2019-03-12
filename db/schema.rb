d3a71c13b4a84262494334c6e6729110083e60d9
Luis <sdaewa22@gmail.com>
2019-03-12 14:19:39 +0000

    ?

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_08_111836) do
+ActiveRecord::Schema.define(version: 2019_03_12_111208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 2019_03_08_111836) do
     t.string "status", default: "Pending"
     t.text "description"
     t.bigint "chat_rooms_id"
+    t.string "state"
+    t.integer "amount_cents", default: 0, null: false
+    t.jsonb "payment"
     t.index ["chat_rooms_id"], name: "index_bookings_on_chat_rooms_id"
     t.index ["client_id"], name: "index_bookings_on_client_id"
     t.index ["therapist_id"], name: "index_bookings_on_therapist_id"
@@ -46,6 +49,12 @@ ActiveRecord::Schema.define(version: 2019_03_08_111836) do
     t.datetime "updated_at", null: false
     t.index ["chat_room_id"], name: "index_chatroom_participants_on_chat_room_id"
     t.index ["user_id"], name: "index_chatroom_participants_on_user_id"
+  end
+
+  create_table "languages", force: :cascade do |t|
+    t.string "language_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "messages", force: :cascade do |t|
@@ -70,11 +79,24 @@ ActiveRecord::Schema.define(version: 2019_03_08_111836) do
 
   create_table "reviews", force: :cascade do |t|
     t.text "description"
+    t.bigint "booking_id"
+    t.bigint "client_id"
     t.bigint "therapist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "rating"
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+    t.index ["client_id"], name: "index_reviews_on_client_id"
     t.index ["therapist_id"], name: "index_reviews_on_therapist_id"
+  end
+
+  create_table "user_languages", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "language_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["language_id"], name: "index_user_languages_on_language_id"
+    t.index ["user_id"], name: "index_user_languages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -89,19 +111,26 @@ ActiveRecord::Schema.define(version: 2019_03_08_111836) do
     t.string "last_name"
     t.text "address"
     t.string "phone_number"
-    t.string "languages"
     t.string "expertise"
-    t.text "bookings"
+    t.bigint "booking_id"
     t.string "photo"
     t.boolean "therapist"
+    t.bigint "review_id"
     t.float "latitude"
     t.float "longitude"
+    t.string "gender"
+    t.text "bio"
+    t.integer "price_cents", default: 0, null: false
     t.string "price", default: "£0"
+    t.index ["booking_id"], name: "index_users_on_booking_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["review_id"], name: "index_users_on_review_id"
   end
 
   add_foreign_key "bookings", "chat_rooms", column: "chat_rooms_id"
+  add_foreign_key "bookings", "users", column: "client_id"
+  add_foreign_key "bookings", "users", column: "therapist_id"
   add_foreign_key "chat_rooms", "bookings"
   add_foreign_key "chatroom_participants", "chat_rooms"
   add_foreign_key "chatroom_participants", "users"
@@ -109,4 +138,9 @@ ActiveRecord::Schema.define(version: 2019_03_08_111836) do
   add_foreign_key "messages", "users"
   add_foreign_key "requests", "chat_rooms"
   add_foreign_key "requests", "users"
+  add_foreign_key "reviews", "bookings"
+  add_foreign_key "reviews", "users", column: "client_id"
+  add_foreign_key "reviews", "users", column: "therapist_id"
+  add_foreign_key "user_languages", "languages"
+  add_foreign_key "user_languages", "users"
 end
